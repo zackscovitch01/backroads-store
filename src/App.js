f9e748f814be0363f85a9afccbeae12947e239e4
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-21 06:59:14 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Tours from "./Components/Tours";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Hero />
+      <About></About>
+      <Services></Services>
+      <Tours></Tours>
+      <Footer></Footer>
+    </>
   );
 }
 
