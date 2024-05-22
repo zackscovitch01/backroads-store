19ed3e5b124d15459ec8313cf6abebc5980eea14
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-22 06:31:27 -0400

    title component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,9 @@
 import aboutImg from "../images/about.jpeg";
+import { Title } from "./Title.js";
 const About = () => {
   return (
     <section className="section" id="about">
-      <div className="section-title">
-        <h2>
-          about <span>us</span>
-        </h2>
-      </div>
+      <Title title="about" subTitle="us"></Title>
 
       <div className="section-center about-center">
         <div className="about-img">
---
+++ src/Components/Title.js
@@ -0,0 +1,9 @@
+export const Title = ({ title, subTitle }) => {
+  return (
+    <div className="section-title">
+      <h2>
+        {title} <span>{subTitle}</span>
+      </h2>
+    </div>
+  );
+};
