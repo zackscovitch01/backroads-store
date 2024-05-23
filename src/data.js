30a89714767024ce134702fe6224e6a7ac488474
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-23 06:16:11 -0400

    tours + footer

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
   { id: 3, href: "#services", text: "services" },
-  { id: 4, href: "#about", text: "about" },
+  { id: 4, href: "#tours", text: "tours" },
 ];
 
 export const socialLinks = [
